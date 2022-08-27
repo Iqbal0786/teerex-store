011944812ffab0d35a04130edc39972376cb736c
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-27 11:40:57 +0530

    added all filter

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -20,18 +20,56 @@ export default function ProductList() {
       
       <div className="contentDiv">
         <div className='filter_div'>
+             {/* color filter box */}
             <div className="checkBoxDiv">
                 <h4>Color</h4>
                 <div className="check_box">
-                <input type="checkbox" name=""  /> <label htmlFor="">Red</label>
+                <input type="checkbox" name=""  value={'Red'} /> <label htmlFor="">Red</label>
                 </div>
                 <div className="check_box">
-                <input type="checkbox" name=""  /> <label htmlFor="">Blue</label>
+                <input type="checkbox" name=""  value={"Blue"} /> <label htmlFor="">Blue</label>
                 </div>
                 <div className="check_box">
-                <input type="checkbox" name=""  /> <label htmlFor="">Green</label>
+                <input type="checkbox" name=""  value={'Green'} /> <label htmlFor="">Green</label>
                 </div>
             </div>
+              {/* gender filter box */}
+              <div className="checkBoxDiv">
+                <h4>Gender</h4>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={'Men'} /> <label htmlFor="">Men</label>
+                </div>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={"Women"} /> <label htmlFor="">Women</label>
+                </div>
+            </div>
+            {/* price filter box */}
+            <div className="checkBoxDiv">
+                <h4>Price</h4>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={'0-250'} /> <label htmlFor="">0- Rs 250</label>
+                </div>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={"251-450"} /> <label htmlFor="">251- Rs 450</label>
+                </div>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={'450'} /> <label htmlFor=""> Rs 450</label>
+                </div>
+            </div>
+             {/* type filter div */}
+             <div className="checkBoxDiv">
+                <h4>Type</h4>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={'Polo'} /> <label htmlFor="">Polo</label>
+                </div>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={"Hoodie"} /> <label htmlFor="">Hoodie</label>
+                </div>
+                <div className="check_box">
+                <input type="checkbox" name=""  value={'Basic'} /> <label htmlFor="">Basic</label>
+                </div>
+            </div>
+             
         </div>
         <div className='product_list_div'></div>
       </div>
