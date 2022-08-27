3868013b50688e6bbd938d96b2f6013e29588778
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-27 09:55:02 +0530

    added navbar

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import "../Styles/styles.css"
 export default function ProductList() {
   return (
-    <div>ProductList</div>
+    <>
+    <div id="navBar">
+      <div className="menu">
+        <h3>TeeRex Store</h3>
+      </div>
+    </div>
+    </>
   )
 }
