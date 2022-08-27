f791292ace389e94aeafb096b67c22a9a375b6fe
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-27 10:40:20 +0530

    added search bar

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,8 +6,20 @@ export default function ProductList() {
     <div id="navBar">
       <div className="menu">
         <h3>TeeRex Store</h3>
+        <i class="fa fa-shopping-cart" aria-hidden="true" style={{fontSize:"35px"}}>
+            <span id='cart_item_count'>0</span>
+        </i>
+    
+         
+      
       </div>
     </div>
+     <div className="searchDiv">
+        <input type="text" placeholder='Search for products...' />
+         <div className="searchIconDiv">
+         <i class="fa fa-search" aria-hidden="true"></i>
+         </div>
+     </div>
     </>
   )
 }
