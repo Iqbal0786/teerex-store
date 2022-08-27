2cad51951e03db070b784732c0d401a577a0a537
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-27 12:03:52 +0530

    added css in single product

--- src/components/SingleProductCard.jsx
+++ src/components/SingleProductCard.jsx
@@ -4,7 +4,12 @@ export default function SingleProductCard() {
   return (
     <>
      <div className="product_card">
-
+        <p className='product_title'>Hoodie</p>
+        <img src="https://m.media-amazon.com/images/I/61oIQ4sgayL._UL1333_.jpg" alt="" />
+         <div className="item_details">
+            <p>Rs 250</p>
+            <button>Add to Cart</button>
+         </div>
      </div>
     
     </>
