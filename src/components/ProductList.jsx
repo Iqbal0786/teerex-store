ee562cea8ca2e64a707208c66a657c7cd9720323
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-27 10:59:34 +0530

    added content div

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -9,17 +9,19 @@ export default function ProductList() {
         <i class="fa fa-shopping-cart" aria-hidden="true" style={{fontSize:"35px"}}>
             <span id='cart_item_count'>0</span>
         </i>
-    
-         
-      
       </div>
     </div>
      <div className="searchDiv">
         <input type="text" placeholder='Search for products...' />
          <div className="searchIconDiv">
-         <i class="fa fa-search" aria-hidden="true"></i>
+            <i class="fa fa-search" aria-hidden="true"></i>
          </div>
      </div>
+      
+      <div className="contentDiv">
+        <div className='filter_div'></div>
+        <div className='product_list_div'></div>
+      </div>
     </>
   )
 }
