20f0d379a0acd96bf0ebb9608390da7911f150e9
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-28 15:01:46 +0530

    added nav bar in cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import "../Styles/styles.css"
 export default function Cart() {
   return (
-    <div>Cart</div>
+    <>
+    <div id="navBar">
+      <div className="menu">
+        <h3>Cart</h3>
+        <i class="fa fa-shopping-cart" aria-hidden="true" style={{fontSize:"35px" , cursor:"pointer"}}
+         onClick={()=>{
+         }}
+        >
+            <span id='cart_item_count'>0</span>
+        </i>
+      </div>
+    </div>
+    <div className="cart_main">
+       
+
+    </div>
+    
+    </>
   )
 }
