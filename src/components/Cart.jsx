9443c9bed0b25d38bf9624d474b7d0623de1b38e
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-28 15:42:37 +0530

    updated cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,14 +10,32 @@ export default function Cart() {
          onClick={()=>{
          }}
         >
-            <span id='cart_item_count'>0</span>
+            <span id='cart_item_count'>{"0"}</span>
         </i>
       </div>
     </div>
     <div className="cart_main">
-       
-
+       <p>Shopping Cart</p>
+       <div className="cart_content">
+          <div className="cart_content_main">
+              <img src="https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/polo-tshirts.png" alt="" />
+               <div className="price_and_title_box">
+                <p>Black Polo</p>
+                <h4>Rs 450</h4>
+               </div>
+               <div className="button_box">
+                 <div className="increase_decrease_box">
+                  <button>+</button>
+                  <div>0</div>
+                  <button>-</button>
+                 </div>
+                  <button>Delete</button>
+               </div>
+          </div>
+        </div>
     </div>
+
+     
     
     </>
   )
